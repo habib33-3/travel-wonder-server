1c9cbd2ee1718b2be1d0d117df154af4e8b65f93
Habibur Rahman <habib.swi16@gmail.com>
2023-11-23 23:36:53 +0600

    connected to mongdb

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import "dotenv/config";
+import { MongoClient, ServerApiVersion } from "mongodb";
 
 const port = process.env.PORT || 5000;
 
@@ -17,3 +19,30 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server at ${port}`);
 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdjrxf4.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
