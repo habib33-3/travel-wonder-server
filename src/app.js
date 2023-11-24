67784f3487f5ab259ba1f96027ccfb388e5862c7
Habibur Rahman <habib.swi16@gmail.com>
2023-11-25 01:38:13 +0600

    create and delete token

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ import cors from "cors";
 import morgan from "morgan";
 import "dotenv/config";
 import { MongoClient, ServerApiVersion } from "mongodb";
+import jwt from "jsonwebtoken";
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -35,6 +36,37 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+
+    const database = client.db("travelWonderDB");
+    const userCollection = database.collection("users");
+
+    app.post("/api/v1/auth/createToken", async (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "30d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("logging out", user);
+      res
+        .clearCookie("token", {
+          maxAge: 0,
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
