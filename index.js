e98ef12cc4605890997c10afb817a944838bf4a1
Habibur Rahman <habib.swi16@gmail.com>
2023-11-28 15:33:36 +0600

    get api for all packages

--- index.js
+++ index.js
@@ -241,6 +241,13 @@ async function run() {
       }
     );
 
+    // get all packages
+    app.get("/api/v1/packages/getAllPackages", async (req, res) => {
+      const result = await toursCollection.find().toArray();
+
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
